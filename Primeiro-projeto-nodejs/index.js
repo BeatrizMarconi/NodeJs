550279ed763ec2175d964a943f08e36a95a3a6c8
BeatrizMarconi <beatrizmarconi.dev@gmail.com>
2022-07-27 14:32:20 -0300

    criando regex

--- Primeiro-projeto-nodejs/index.js
+++ Primeiro-projeto-nodejs/index.js
@@ -1,19 +1,31 @@
-// const chalk = require('chalk');
 const fs = require('fs');
 
-function trataErro (erro) {
-    throw new Error(`${erro.code} vish deu erro em!`);
-}
+const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)';
 
-async function pegaArquivo(caminho){
-    const encoding = 'utf-8';
-    try {
-        const texto = await fs.promises.readFile(caminho, encoding)
-        console.log(texto)
-    } catch(erro) {
-        trataErro(erro)
-    }
+function extraiLink(texto){
+    const regex = /\[([^\]]*)\]\(https?:\/\/[^$#\s].[^\s]*\)/gm;
+    const resultado = [];
+
+    // while()
+
+    const linksExtraidos = regex.exec(texto);
+    console.log(linksExtraidos);
 }
+extraiLink(texto);
+
+// function trataErro (erro) {
+//     throw new Error(`${erro.code} vish deu erro em!`);
+// }
+
+// async function pegaArquivo(caminho){
+//     const encoding = 'utf-8';
+//     try {
+//         const texto = await fs.promises.readFile(caminho, encoding)
+//         console.log(texto)
+//     } catch(erro) {
+//         trataErro(erro)
+//     }
+// }
 
 // function pegaArquivo(caminho){
 //     const encoding = 'utf-8';
@@ -32,4 +44,4 @@ async function pegaArquivo(caminho){
 //     })
 // }
 
-pegaArquivo('./arquivos/texto1.md');
+// pegaArquivo('./arquivos/texto1.md');
